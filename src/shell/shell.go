ba31e78266548d5d62d16a4b999c1377071fd309
yes <wuxiaobin@wuzhoucloud.com>
2021-04-26 11:37:38 +0800

    feat: add shell

--- src/shell/shell.go
+++ src/shell/shell.go
@@ -1,27 +1,21 @@
-package main
+package shell
 
 import (
 	"bytes"
-	"fmt"
 	"os/exec"
 )
 
-func run_command(str string) (string, error) {
-	cmd := exec.Command(str)
+func RunCommand(str string, shell bool) (string, error) {
+	var cmd *exec.Cmd
+	if shell == true {
+		cmd = exec.Command("/bin/sh", "-c", str)
+	} else {
+		cmd = exec.Command(str)
+	}
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	outStr := string(stdout.String())
 	return outStr, err
-}
-
-func main() {
-	var out, err = run_command("tasklist")
-	if err == nil {
-		fmt.Println(out)
-	} else {
-		fmt.Println(err.Error())
-	}
-
 }
